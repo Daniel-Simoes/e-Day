e3fa47fdd8c847627b8b7ca72bce2c017374a402
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-11 18:37:51 +0100

    MongoDB Config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://eday:eday@eday-tvuy0.mongodb.net/eday?retryWrites=true&w=majority', {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+})
+
 
 app.use(express.json());
 app.use(routes);
