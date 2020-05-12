6717c25274138ebe9788cf09907aa4bf9a743c35
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-12 21:08:14 +0100

    Send Company Register by WEB to DB

--- web/src/pages/NewRegister/index.js
+++ web/src/pages/NewRegister/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 import camera from '../../assets/camera.png';
 
 import './styles.css';
@@ -11,10 +12,22 @@ export default function New({ history }) {
     const preview = useMemo(() => {
       return thumbnail ? URL.createObjectURL(thumbnail) : null;
   }, [thumbnail])
-    
+     
+    async function handleSubmit(event) {
+      event.preventDefault();
+      
+      const data = new FormData();
+      const user_id = localStorage.getItem('user');
 
-    async function handleSubmit() {
-       
+      data.append('thumbnail', thumbnail);
+      data.append('company', company);
+      data.append('techs', techs);
+
+      await api.post('/spots', data, {
+        headers: { user_id }
+    });
+
+    history.push('/profile');
     }
 
     return (
@@ -24,13 +37,13 @@ export default function New({ history }) {
                 style={{ backgroundImage: `url(${preview})`}}
                 className={thumbnail ? 'has-thumbnail' : ''}>
                 <input type="file" onChange={event => setThumbnail(event.target.files[0])}/>
-                <img className="imgUpdate"src={camera} alt="Select img" />
+                <img className="imgUpdate" src={camera} alt="Select img" />
               </label>
 
             <label htmlFor="company">COMPANY *</label>
             <input
                 id="company"            
-                placeholder="company name"
+                placeholder="name"
                 value={company}
                 onChange={event => setCompany(event.target.value)}
             />
@@ -38,11 +51,10 @@ export default function New({ history }) {
             <label htmlFor="techs">TECHNOLOGIES * <span>(comma-separated)</span></label>
             <input
                 id="techs"            
-                placeholder="what technologies use"
+                placeholder="what tech use"
                 value={techs}
                 onChange={event => setTechs(event.target.value)}
             />
-
             <button type="submit" className="btn">Register</button>
         </form>
     )
