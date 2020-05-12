c4df8b88c8a988705a2af369119615a0e543d8b2
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-12 17:26:00 +0100

    fix backend to allow frontend to show upload file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); 
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +14,7 @@ mongoose.connect('mongodb+srv://eday:eday@eday-tvuy0.mongodb.net/eday?retryWrite
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
