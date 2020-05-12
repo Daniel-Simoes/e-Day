390e6c782dfaa8c83e5684b005a2bb502fb8a809
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-12 16:44:19 +0100

    Call-Company that was registed by user(Profile)

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+import './styles.css'
 
-function Dashboard() {
-  return <div />;
+export default function Dashboard() {
+    useEffect(() => {
+        async function loadSpots() {
+            const user_id = localStorage.getItem('user');
+            const response = await api.get('/profile', {
+                headers: { user_id }
+            });
+
+            console.log(response.data);
+        }     
+        loadSpots();
+    }, []);
+
+    return <h1>Dashboard</h1>
 }
-
-export default Dashboard;
