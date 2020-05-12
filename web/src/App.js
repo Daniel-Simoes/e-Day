85ba78ac845cf61e287268310025d90eae62818f
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-12 14:19:40 +0100

    Login Page

--- web/src/App.js
+++ web/src/App.js
@@ -2,11 +2,37 @@ import React from 'react';
 
 import './App.css';
 
+import logo from './assets/logo.png';
+
 function App() {
+  
   return (
-    <div>
-      <h1>Hello e'Day!</h1>
-    </div>
+    <div className="container">
+      <img src={logo} alt="e'Day" />
+    
+
+    <div className="content">
+      <p>
+      Introduce your  <strong>Company</strong> to new  <strong>talent</strong> and win an admirer.
+      </p>
+
+      <form>
+      <label htmlFor="email">E-MAIL *</label>
+      <input
+          type="email"
+          id="email"
+          placeholder="email"
+          
+      />
+      
+
+      <button type="submit" className="btn">Enter</button>
+      </form>
+      <div className="footer">
+        <p>*A valide email is requeire</p>
+      </div>
+      </div>
+      </div>
   );
 }
 
