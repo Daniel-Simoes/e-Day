75199abd7d683426da1d59ec7669e1ca70b737eb
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-13 19:40:55 +0100

    Fixed bug from navigation between pages

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -9,11 +9,19 @@ import Background from '../../assets/background.jpg';
 
 const image = Background
 
-export default function Login() {
+export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [techs, setTechs] = useState('');
 
-  async function handleSubmit({ navigation }) {
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+        if (user) {
+            navigation.navigate('Dashboard');
+        }
+    })
+}, []);
+
+  async function handleSubmit() {
     const response = await api.post('/sessions', {
         email
     })
