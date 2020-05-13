12ad7f7e84ccb303345cfca1b55b196089df4054
Daniel-Simoes <daniel.simoes@hotmail.com>
2020-05-13 21:03:02 +0100

    Dashboard Layout

--- mobile/src/pages/Dashboard/index.js
+++ mobile/src/pages/Dashboard/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, ImageBackground, StyleSheet, Image, AsyncStorage } from 'react-native';
 
-// import { Container } from './styles';
+import SpotList from '../../components/SpotList';
 
-export default function Dashboard() {
-  return <View><Text>Dashboard</Text></View>;
+import logo from '../../assets/logo.png';
+import Background from '../../assets/background.jpg';
+
+const image = Background
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+      <>
+      <ImageBackground source={image} style={styles.image} opacity="0.3">
+        <SafeAreaView style={styles.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+              {techs.map(tech => <SpotList key={tech} tech={tech} /> )}
+            </ScrollView>
+        </SafeAreaView>
+      </ImageBackground>
+      </>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+
+    logo: {
+        height: 70,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    },
+    image: {
+      flex: 1,
+      resizeMode: "cover",
+      justifyContent: "center",
+    },
+})
